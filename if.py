9f230ef0ffd1adc6e4deee24c03fe437ffeeee2c
NicholasBoss <NicholasBoss0914@gmail.com>
2024-01-25 11:00:35 -0700

    added a while loop to if.py

--- if.py
+++ if.py
@@ -1,9 +1,13 @@
 print('Left or Right (L/R)?')
 direction = input()
+number = 10
 if direction.lower() == 'l':
     print('You are going to the left.')
 elif direction.lower() == 'r':
     print('You are going to the right.')
 else:
     print('You did not enter L or R!')
+    while number != 0:
+        print(number)
+        number -= 1
     
